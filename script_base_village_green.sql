e3da85583f8d4d4633ee1afd5ad402d855544845
Matthieu Gueulle <gueulle.matthieu60@gmail.com>
2021-05-10 16:54:59 +0200

    Création du script SQL
    Création script SQL pour VillageGreen.

---
+++ script_base_village_green.sql
@@ -0,0 +1,152 @@
+
+--  BASE VillageGreen --
+
+-- CREATION DE LA BASE --
+
+DROP DATABASE IF EXISTS villagegreen;
+
+CREATE DATABASE villagegreen CHARACTER SET 'utf8mb4';
+
+USE villagegreen;
+
+-- CREATION DE LA TABLE categories --
+
+CREATE TABLE `categories` (
+     `cat_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `cat_name` VARCHAR(50) NOT NULL,
+     `cat_parent` INT UNSIGNED
+)
+ENGINE = innoDB;
+
+-- CREATION DE LA TABLE suppliers --
+
+CREATE TABLE `suppliers` (
+     `sup_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `sup_type` VARCHAR(15),
+     `sup_address` VARCHAR(255) NOT NULL,
+     `sup_postalcode` INT NOT NULL,
+     `sup_city` VARCHAR(30) NOT NULL,
+     `sup_contact` VARCHAR(30) NOT NULL,
+     `sup_phone` INT NOT NULL,
+     `sup_mail` VARCHAR(255) NOT NULL
+)
+ENGINE = innoDB;
+
+-- CREATION DE LA TABLE products --
+
+CREATE TABLE `products` (
+     `pro_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `pro_ref` VARCHAR(15) NOT NULL,
+     `pro_label` VARCHAR(30) NOT NULL,
+     `pro_desc` VARCHAR(255) NOT NULL,
+     `pro_ppet` DECIMAL(7,2) NOT NULL,
+     `pro_spet` DECIMAL(7,2) NOT NULL,
+     `pro_photo` VARCHAR(255),
+     `pro_phy_stk` INT NOT NULL,
+     `pro_lock` INT NOT NULL,
+     `pro_add_date` DATETIME NOT NULL,
+     `pro_modif_date` DATE DEFAULT NULL,
+     `pro_sup_id` INT NOT NULL,
+     `pro_cat_id` INT NOT NULL,
+     FOREIGN KEY (`pro_sup_id`) REFERENCES `suppliers`(`sup_id`),
+     FOREIGN KEY (`pro_cat_id`) REFERENCES `categories`(`cat_id`)
+)
+ENGINE = innoDB;
+
+-- CREATION DE LA TABLE sales_representants --
+
+CREATE TABLE `sales_representants` (
+    `rep_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `rep_lastname` varchar(50) NOT NULL,
+    `rep_firstname` varchar(50) NOT NULL
+)
+ENGINE = innoDB;
+
+-- CREATION DE LA TABLE customers --
+
+CREATE TABLE `customers` (
+     `cus_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `cus_lastname` VARCHAR(50) NOT NULL,        
+     `cus_firstname`   VARCHAR(30) NOT NULL,      
+     `cus_sex`   INT NOT NULL,              
+     `cus_bil_address` VARCHAR(255)  NOT NULL,
+     `cus_bil_postalcode` INT NOT NULL,       
+     `cus_bil_city`  VARCHAR(30) NOT NULL,   
+     `cus_del_address` VARCHAR(255)  NOT NULL, 
+     `cus_del_postalcode` INT NOT NULL,        
+     `cus_del_city`  VARCHAR(30) NOT NULL,        
+     `cus_phone`   INT NOT NULL,         
+     `cus_mail`   VARCHAR(255) NOT NULL,          
+     `cus_type`   VARCHAR(15) NOT NULL,            
+     `cus_coef`   DECIMAL (3,2) NOT NULL,
+     `cus_rep_id` INT NOT NULL,
+     FOREIGN KEY (`cus_rep_id`) REFERENCES `sales_representants`(`rep_id`)
+)
+ENGINE = innoDB;
+
+-- CREATION DE LA TABLE order_status --
+
+CREATE TABLE `order_status` (
+    `ost_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `ost_label` varchar(20) NOT NULL
+)
+ENGINE = innoDB;
+
+-- CREATION DE LA TABLE orders --
+
+CREATE TABLE `orders` (
+    `ord_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `ord_date` DATE NOT NULL,
+    `ord_discount` DECIMAL(4,2),
+    `ord_pay_method` VARCHAR(10) NOT NULL,
+    `ord_ost_id` INT NOT NULL,
+    `ord_cus_id` INT NOT NULL,
+     FOREIGN KEY (`ord_ost_id`) REFERENCES `order_status`(`ost_id`),
+     FOREIGN KEY (`ord_cus_id`) REFERENCES `customers`(`cus_id`)
+)
+ENGINE = innoDB;
+
+-- CREATION DE LA TABLE bills --
+
+CREATE TABLE `bills` (
+     `bil_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `bil_date` INT NOT NULL,
+     `bil_ord_id` INT NOT NULL,
+     FOREIGN KEY (`bil_ord_id`) REFERENCES `orders`(`ord_id`)
+)
+ENGINE = innoDB;
+
+-- CREATION DE LA TABLE deliveries --
+
+CREATE TABLE `deliveries` (
+     `del_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `del_date` DATE NOT NULL,
+     `del_ord_id` INT NOT NULL,
+     FOREIGN KEY (`del_ord_id`) REFERENCES `orders`(`ord_id`)
+)
+ENGINE = innoDB;
+
+-- CREATION DE LA TABLE order_details --
+
+CREATE TABLE `order_details` (
+     `ode_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `ode_qty` INT NOT NULL,
+     `ode_tot_exc_tax` DECIMAL(7,2) NOT NULL,
+     `ode_tax_rate` DECIMAL(4,2) NOT NULL,
+     `ode_tot_all_tax_inc` DECIMAL(7,2) NOT NULL,
+     `ode_pro_id` INT NOT NULL,
+     `ode_ord_id` INT NOT NULL,
+     FOREIGN KEY (`ode_pro_id`) REFERENCES `products`(`pro_id`),
+     FOREIGN KEY (`ode_ord_id`) REFERENCES `orders`(`ord_id`)
+)
+ENGINE = innoDB;
+
+-- CREATION DE LA TABLE delivery_details --
+
+CREATE TABLE `delivery_details` (
+     `dde_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `dde_qty` INT NOT NULL,
+     `ode_del_id` INT NOT NULL,
+     FOREIGN KEY (`ode_del_id`) REFERENCES `order_details`(`ode_id`)
+)
+ENGINE = innoDB;
