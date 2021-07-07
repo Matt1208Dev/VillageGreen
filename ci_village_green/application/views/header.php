7306192fe8a8193b89fae3b3403b2d653602b7e0
Matthieu Gueulle <gueulle.matthieu60@gmail.com>
2021-07-07 16:57:17 +0200

    debut front-office
    parametrage CodeIgniter
    démarrage view header

---
+++ ci_village_green/application/views/header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css')?>">
+    <title>Village Green, des musiciens au service des musiciens</title>
+</head>
+<body>
+    
+<div class="container"> <!-- Ouverture du container principal -->
+
+    <nav class="navbar p-0">
+        <div class="container-fluid navbar p-0">
+            <div class="row">
+                <div class="col-md-4">
+                    <a class="navbar-brand" href="<?php echo site_url('Produits/accueil');?>">
+                        <img src="<?php echo base_url('assets/images/HEADER/logo village green.png');?>" alt="Logo Village Green" title="Logo Village Green">
+                    </a>
+                </div>
+
+                <div class="col-md-8">
+                    <div class="row" id="nav-first-row">
+                        <div class="container navbar d-flex justify-content-end p-0" id="nav-first-row-div">
+                                <a class="nav-link" href="#">Infos</a>
+                                <a class="nav-link" href="#">Espace Client</a>
+                                <a class="nav-link" href="#">
+                                    <img class="img-fluid" src="<?php echo base_url('assets/images/HEADER/picto panier.png');?> " alt="pictogramme panier d'achat" title="Panier d'achat">
+                                </a>
+                                <a class="nav-link" href="#">
+                                    <img src="<?php echo base_url('assets/images/HEADER/picto pays.png');?> " alt="pictogramme pays" title="Langue de navigation">
+                                </a>
+                        </div>
+                    </div>
+
+                    <div class="row" id="nav-second-row">
+                        <div class="container navbar d-flex justify-content-around p-1">
+                            <a class="nav-link fw-bold py-1" href="#">Produits</a>
+                            <a class="nav-link fw-bold py-1" href="#">Services</a>
+                            <a class="nav-link fw-bold py-1" href="#">Aide</a>
+                            <a class="nav-link fw-bold py-1" href="#">A Propos</a>
+                        </div>
+                    </div>
+
+                    <div class="row" id="nav-third-row">
+                        <div class="container navbar d-flex justify-content-around p-1">
+                            <a class="nav-link pr-1 py-1" href="#">Guit/Bass</a>
+                            <a class="nav-link px-1 py-1" href="#">Batteries</a>
+                            <a class="nav-link px-1 py-1" href="#">Clavier</a>
+                            <a class="nav-link px-1 py-1" href="#">Studio</a>
+                            <a class="nav-link px-1 py-1" href="#">Sono</a>
+                            <a class="nav-link px-1 py-1" href="#">Eclairages</a>
+                            <a class="nav-link px-1 py-1" href="#">DJ</a>
+                            <a class="nav-link px-1 py-1" href="#">Cases</a>
+                            <a class="nav-link px-1 py-1" href="#">Accessoires</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
